222d073a175dd01d8690a6497b07ff78c83f97a3
Dylan Foster <dylantf@gmail.com>
2017-10-12 18:58:04 -0400

    updates for new api

--- ExportSLDataToApps/Program.cs
+++ ExportSLDataToApps/Program.cs
@@ -16,7 +16,7 @@ namespace ExportSLDataToApps
       {
          var companies = new List<string>();
          companies.Add("ari");
-         companies.Add("agt");
+         //companies.Add("agt");
 
          foreach (var company in companies)
          {
@@ -34,9 +34,11 @@ namespace ExportSLDataToApps
             // Get list of ARI projects and cost codes
             Console.WriteLine("Getting list of projects from SL...");
             var projects = Project.FetchActiveProjects(source);
+            Console.WriteLine("Found {0} projects.", projects.Count);
 
             Console.WriteLine("Getting list of cost codes from SL...");
             var costCodes = CostCode.FetchCostCodes(source);
+            Console.WriteLine("Found {0} cost codes.", costCodes.Count);
             Project.MapCostCodesToProjectList(projects, costCodes);
 
             // Send mapped project list to API
@@ -47,13 +49,15 @@ namespace ExportSLDataToApps
 
             var client = new RestClient(apiBaseUrl);
             client.Authenticator = new HttpBasicAuthenticator(apiUsername, apiPassword);
-            var request = new RestRequest(company, Method.POST);
+            var request = new RestRequest(Method.POST);
             var requestBody = JsonConvert.SerializeObject(projects);
+            request.AddHeader("Content-Type", "application/json");
             request.AddParameter("data", requestBody);
 
             try
             {
                var response = client.Execute(request);
+               Console.WriteLine("Response code: {0} {1}", response.ResponseStatus, (int) response.StatusCode);
                Console.WriteLine(response.Content);
             }
             catch (Exception e)
@@ -64,7 +68,7 @@ namespace ExportSLDataToApps
          }
                   
          //Console.WriteLine("Press any key to exit...");
-         //Console.ReadKey();
+         Console.ReadKey();
       }
    }
 }
